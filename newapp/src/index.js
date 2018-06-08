f6dd32d2883c6b4e17f048883af78440eecd758f
vikas agarwal <vagarwal@sumologic.com>
2018-06-08 12:43:42 +0530

    .

--- newapp/src/index.js
+++ newapp/src/index.js
@@ -5,16 +5,20 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state= {
-            inputs: []
+            inputs: [],
+            value : "",
+            temp :"Enter Feedback :  "
          }
 
             this.handleclick =this.handleclick.bind(this)
             this.deleteitem = this.deleteitem.bind(this)
+            this.changevalue = this.changevalue.bind(this)
+            this.handleSubmit = this.handleSubmit.bind(this)
         }
 
     handleclick()
     {
-        const val = document.getElementById("input").value;
+        const val = this.state.value;
         if(val.length>0 && this.state.inputs.indexOf(val)===-1)
         {
             const newinputs = this.state.inputs;
@@ -25,7 +29,7 @@ class App extends React.Component{
 
     deleteitem()
     {
-        const val = document.getElementById("input").value;
+        const val = this.state.value;
         if(val.length>0 && this.state.inputs.indexOf(val)!==-1)
         {
             const newinputs = this.state.inputs;
@@ -34,16 +38,36 @@ class App extends React.Component{
         }
     }
 
+    changevalue(event)
+    {
+        const target = event.target;
+        const val = target.value;
+        const name= target.name ;
+        console.log(name);
+        this.setState({ [name] : val});
+    }
 
+
+    handleSubmit(event)
+    {
+        event.preventDefault();
+    }
     render(){
         return(
 
         <div>
 
-            <input type="text" id="input" />
+            <input type={"text"} name="value" id="input" value={this.state.value} onChange={this.changevalue} />
             <button onClick={this.handleclick}> Add </button>
             <button onClick={this.deleteitem}>Delete</button>
+            <p/>
+
             <Printlist inputs={this.state.inputs} />
+            <p/>
+            <textarea name="temp" value={this.state.temp} onChange={this.changevalue}></textarea>
+
+            <p></p>
+
         </div>
         )
     }
