67845598ea49e7e89f07b071e3d4dfe2616a78ae
vikas agarwal <vagarwal@sumologic.com>
2018-06-08 10:11:55 +0530

    simple react app

--- newapp/src/index.js
+++ newapp/src/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            inputs: []
+         }
+
+            this.handleclick =this.handleclick.bind(this)
+            this.deleteitem = this.deleteitem.bind(this)
+        }
+
+    handleclick()
+    {
+        const val = document.getElementById("input").value;
+        if(val.length>0 && this.state.inputs.indexOf(val)===-1)
+        {
+            const newinputs = this.state.inputs;
+            newinputs.push(val);
+            this.setState({inputs : newinputs});
+        }
+    }
+
+    deleteitem()
+    {
+        const val = document.getElementById("input").value;
+        if(val.length>0 && this.state.inputs.indexOf(val)!==-1)
+        {
+            const newinputs = this.state.inputs;
+            newinputs.splice(newinputs.indexOf(val),1);
+            this.setState({inputs : newinputs});
+        }
+    }
 
 
-function welcome(props){
-       return  <h1>hello,{props.name}</h1>;
+    render(){
+        return(
+
+        <div>
+
+            <input type="text" id="input" />
+            <button onClick={this.handleclick}> Add </button>
+            <button onClick={this.deleteitem}>Delete</button>
+            <Printlist inputs={this.state.inputs} />
+        </div>
+        )
+    }
+}
+
+
+function Printlist(props){
+
+    const text = props.inputs.map((val,index)=><li key={val}><strong>{index+1}.</strong> {val}</li> );
+    return (
+        <ul>
+            {text}
+        </ul>
+    );
 
 }
-const element = <welcome name ="vikas" />;
 ReactDOM.render(
-    <welcome name ="vikas" />,
+    <App />,
     document.getElementById('root')
 
-);
+)
