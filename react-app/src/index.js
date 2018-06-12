0123d26f365e6c40eb2923e122fc4bf021fe3d81
vikas agarwal <vagarwal@sumologic.com>
2018-06-12 10:18:39 +0530

    new commit

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -75,10 +75,20 @@ class Inputsearch extends React.Component{
     render(){
         return(
             <div>
-                <input  placeholder="search..." type="text"  value={this.props.text} className="search" onChange={this.props.ontextchange} />
+                <input  placeholder="search..." type="text"
+                        value={this.props.text}
+                        className="search"
+                        onChange={this.props.ontextchange} />
+
+
                 <br/>
                 <label className="checkbox">
-                <input type="checkbox"  checked={this.props.checked}  onChange={this.props.oncheck}/>
+
+                <input type="checkbox"
+                       checked={this.props.checked}
+                       onChange={this.props.oncheck}/>
+
+
                     Only show item in stock
                 </label>
 
